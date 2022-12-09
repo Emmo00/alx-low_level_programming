b41fd16290860d56caf75d7f37590e7ac5273f61
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2022-12-08 21:19:11 -0800

    write function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * write - function
+ *
+ * Description: writes
+ * @filedes: filedes
+ * @buf: buf
+ * @nbyte: no byte
+ *
+ * Return: 0
+ */
+int write(int filedes, const char *buf, unsigned int nbyte);
+/**
+ * main - Entry point
+ *
+ * Description: prints std output without printf or puts
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 13);
+	return (1);
+}
