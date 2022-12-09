1bb746ee7f26c599b1199e38f95853290f102eb1
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2022-12-09 13:15:15 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all letters of the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 48;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
