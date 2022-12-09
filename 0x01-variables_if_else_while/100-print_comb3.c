bba123beb4d9b6a79858b90e3fe1ddc812e71e74
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2022-12-09 14:32:57 -0800

    two digit combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all letters of the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int f = 48;
+	int s = 49;
+	int i;
+	int j;
+	int k;
+
+	for (i = 0, k = 9; i < 9; i++)
+	{
+		s = 49 + i;
+		for (j = k; j > 0; j--)
+		{
+			putchar(f);
+			putchar(s);
+			if (f == 56 && s == 57)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+			s++;
+		}
+		f++;
+		k--;
+	}
+	putchar('\n');
+	return (0);
+}
