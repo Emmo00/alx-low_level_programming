077653d238acff358407d90fe54bd348e1ecd38a
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2022-12-08 21:20:06 -0800

    write function

--- 0x00-hello_world/file.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h> 
- 
-int write(int filedes, const char *buf, unsigned int nbyte); 
-int main( int argc, char** argv ) 
-{ 
-   write(1, "Hello World!\n", 13); 
- 
-   return 0; 
-} 
