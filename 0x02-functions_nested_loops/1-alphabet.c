586c8b4d153e00d550157706460eb2501f8e0430
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2022-12-13 09:14:31 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Description: prints the alphabet with the _putchar function in_putchar.c
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char c = 97;
+
+	for (c = 97; c < 123; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
