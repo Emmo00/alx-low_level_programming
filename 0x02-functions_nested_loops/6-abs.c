42119465c0630549d7d7b40dc1a74d9295e05e4e
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2022-12-14 07:46:25 -0800

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *  _abs - computes absolute value
+ * @n: number input
+ *
+ * Return: result
+ */
+int  _abs(int n)
+{
+	if (n < 0)
+		return (-(n));
+	else
+		return (n);
+}
