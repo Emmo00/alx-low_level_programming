5c6ca513b2e432688756d77e1c4eb926fe3f7c4c
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2022-12-14 08:11:35 -0800

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,8 +11,8 @@ int print_last_digit(int n)
 	int last_digit;
 
 	if (n < 0)
-		n = -(n);
+		n = n * -1;
 	last_digit = n % 10;
-	_putchar(last_digit + 48);
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
