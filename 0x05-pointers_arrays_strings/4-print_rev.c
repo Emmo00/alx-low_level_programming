d050370a6b1aad0edafe0d1228acfb2f94d42c17
Emmanuel <nwaforemmanuel005@gmail.com>
2022-12-20 05:10:47 +0100

    rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,10 +7,13 @@
 void print_rev(char *s)
 {
 	int i;
+	char br;
 
 	i = 0;
+	br = '\n';
 	while (s[i] != '\0')
 		i++;
-	for (; i >= 0; --i)
+	for (i--; i >= 0; i--)
 		write(1, &s[i], 1);
+	write(1, &br, 1);
 }
