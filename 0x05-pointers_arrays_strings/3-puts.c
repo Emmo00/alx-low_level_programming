1b46c82bf346df19b59a80c8bb996f9e36268b77
Emmanuel <nwaforemmanuel005@gmail.com>
2022-12-20 04:31:59 +0100

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,19 @@
-#
+#include<unistd.h>
+/**
+ * _puts - prints to standard output
+ * @str: character string
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int i;
+	char br;
+
+	br = '\n';
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, &br, 1);
+}
