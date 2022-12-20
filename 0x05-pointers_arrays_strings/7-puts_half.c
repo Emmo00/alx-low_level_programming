f3081086342ea620bbec821e0efe44d354c169cd
Emmanuel <nwaforemmanuel005@gmail.com>
2022-12-20 05:54:53 +0100

    half string:

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include<unistd.h>
+/**
+ * puts_half - prints to half of a string
+ * @str: character string
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int i, j;
+	char br;
+
+	i = sizeof(str);
+	if (i % 2 == 0)
+		j = i / 2;
+	else
+		j = (i - 1) / 2
+	br = '\n';
+	while (j <= i)
+	{
+		write(1, &str[j], 1);
+		j++;
+	}
+	write(1, &br, 1);
+}
