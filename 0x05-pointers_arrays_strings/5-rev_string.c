26dc4a541c465ee3c88faf154f16848167c153b2
Emmanuel <nwaforemmanuel005@gmail.com>
2022-12-20 05:25:17 +0100

    rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include<unistd.h>
+/**
+ * rev_string - prints a string in reverse
+ * @s: character string
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int i;
+	char br;
+	char *tmp;
+
+	i = 0;
+	br = '\n';
+	tmp = malloc(sizeof(s));
+	while (s[i] != '\0')
+	{
+		i++;
+		tmp[i] = s[i];
+	}
+	for (i--; i >= 0; i--)
+		s[i] = tmp[i];
+	write(1, &br, 1);
+	free(tmp);
+}
