ff0679a2d3558b6399fe6372935a7b02a9eb8dad
Emmanuel <nwaforemmanuel005@gmail.com>
2022-12-20 04:54:50 +0100

    print string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,21 @@
-/
+#include<unistd.h>
+/**
+ * print_rev - prints a string in reverse
+ * @s: character string
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int i;
+	char br;
+
+	i = 0;
+	br = '\n';
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (;i > 0; i--)
+		write(1, &s[i], 1);
+	write(1, &br, 1);
+}
