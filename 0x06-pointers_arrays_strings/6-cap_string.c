d1123da791ec286197182b72b7f111ee1a41ca50
Emmanuel <nwaforemmanuel005@gmail.com>
2022-12-22 21:19:36 +0100

    cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,14 +5,14 @@
  */
 char *cap_string(char *s)
 {
-	char *spt = " \t\n,;.!?\"(){}";
+	char spt[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 	int i, j;
 
 	i = 0;
 	while (s[i] != '\0')
 	{
 		j = 0;
-		while (spt[j] != '\0')
+		while (j < 13)
 		{
 			if (s[i] == spt[j] && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
 				s[i + 1] -= 32;
