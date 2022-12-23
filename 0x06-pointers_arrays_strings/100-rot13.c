fe1819cb148b9f91ec5375fb77416bf4e6152210
Emmanuel <nwaforemmanuel005@gmail.com>
2022-12-23 01:08:08 +0100

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,19 @@
+/**
+ * rot13 - encodes a string using the rot13
+ * @s: string
+ * Return: encoded string
+ */
+char *rot13(char *s)
+{
+	int i;
+
+	i = 0;
+	do {
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = 97 + ((s[i] - 97) + 13) % 26;
+		if (s[i] >= 'A' && s[i] <= 'Z')
+			s[i] = 65 + ((s[i] - 65) + 13) % 26;
+		i++;
+	} while (s[i] != '\0');
+	return (s);
+}
