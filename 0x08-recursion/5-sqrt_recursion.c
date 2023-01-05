b0ee1f454f7070d1ac5ef3fb56580af9131dba84
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-01-04 16:37:10 -0800

    file template

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+/**
+ * _sqrt_recursion - return the natural square root of a number
+ * @n: number
+ * Return: natutal square root of n, -1 if n does not have
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 1)
+		return (-1);
+	if (n == 1)
+		return (1);
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,8 @@
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: number
+ * Return: 1 if the number is prime, otherwise 0
+ */
+int is_prime_number()
+{
+}
