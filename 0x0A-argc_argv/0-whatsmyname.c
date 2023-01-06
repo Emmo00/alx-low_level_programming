864db4c2cd796f534e6ad63aa0d76ed67d47ae64
Emmanuel <nwaforemmanuel005@gmail.com>
2023-01-06 21:18:26 +0100

    program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,3 @@
-#include<stdio.h>
 #include "main.h"
 /**
  * main - entry point
@@ -8,7 +7,13 @@
  */
 int main(int argc, char *argv[])
 {
+	int i;
+
 	(void) argc;
-	printf("%s\n", argv[0]);
+
+	i = 0;
+	while (argv[0][i])
+		_putchar(argv[0][i++]);
+	_putchar('\n');
 	return (0);
 }
