3a1d3b9b2001f29a16208c33d2ba9d57a1271110
Emmanuel <nwaforemmanuel005@gmail.com>
2023-01-09 16:13:23 +0100

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,9 +14,8 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	arr = malloc((sizeof(char) * size) + 1);
+	arr = malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
 		*(arr + i) = c;
-	*(arr + i) = '\0';
 	return (arr);
 }
