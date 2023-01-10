9731820c2462825c910e93a4d14cc81b325b3578
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-01-10 01:52:00 -0800

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,25 +11,29 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *nstr;
-	unsigned int len1, len2;
+	unsigned int len1, len2, i;
 
 	len1 = 0;
 	len2 = 0;
 	if (s1 != NULL)
 		while (s1[len1])
 			len1++;
+	else
+		s1 = "";
 	if (s2 != NULL)
 		while (s2[len2])
 			len2++;
+	else
+		s2 = "";
 	nstr = (char *)malloc(((len1 + len2) * sizeof(char)) + 1);
 	if (nstr == NULL)
 		return (NULL);
 	if (s2 != NULL)
-		while (*s1)
-			*nstr++ = *s1++;
+		for (i = 0; i < len1; i++)
+			*(nstr + i) = *(s1 + i);
 	if (s2 != NULL)
-		while (*s2)
-			*nstr++ = *s2++;
-	*nstr = '\0';
-	return (nstr - (len1 + len2));
+		for (; i < len2 + len1; i++)
+			*(nstr + i) = *(s2 + (i - len1));
+	*(nstr + i) = '\0';
+	return (nstr);
 }
