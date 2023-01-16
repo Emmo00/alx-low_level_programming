c45070b6c624c416206bef3588fde1d37aa0a801
Emmanuel <nwaforemmanuel005@gmail.com>
2023-01-16 17:51:12 +0100

    new

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,7 +12,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ndog;
 
-	if ( name == NULL || owner == NULL)
+	if (name == NULL || owner == NULL)
 		return (NULL);
 	ndog = malloc(sizeof(*ndog));
 	if (ndog == NULL)
