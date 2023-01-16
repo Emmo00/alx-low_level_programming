d13259c79468c0f4c52f563de4ef6aeb5110619e
Emmanuel <nwaforemmanuel005@gmail.com>
2023-01-16 17:44:16 +0100

    allocate space for the name and owner attribute

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include<stdlib.h>
+#include<strings.h>
 /**
  * new_dog - creates a new dog
  * @name: dog name
@@ -11,11 +12,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ndog;
 
+	if ( name == NULL || owner == NULL)
+		return (NULL);
 	ndog = malloc(sizeof(*ndog));
 	if (ndog == NULL)
 		return (NULL);
+	ndog->name = malloc(sizeof(char) * strlen(name));
+	if (ndog == NULL)
+	{
+		free(ndog);
+		return (NULL);
+	}
 	ndog->name = name;
 	ndog->age = age;
+	ndog->owner = malloc(sizeof(char) * strlen(owner));
+	if (ndog->owner == NULL)
+	{
+		free(ndog->name);
+		free(ndog);
+		return (NULL);
+	}
 	ndog->owner = owner;
 	return (ndog);
 }
