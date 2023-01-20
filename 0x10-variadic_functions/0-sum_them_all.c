9ccfbe2c8eff259dd8e703fb3de120e2f46ad121
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-01-20 16:39:27 +0100

    sum all

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include<stdarg.h>
+/**
+ * sum_them_all - returns the sum of all its parameters
+ * @n: number of parameters to be recieved
+ * @...: parameters
+ * Return: sum  of all
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+    va_list args;
+    unsigned int i, sum;
+
+    va_start(args, n);
+    for (i = 0; i < n; i++)
+    {
+        sum += va_arg(args, int);
+    }
+    va_end(args);
+    return (sum);
+}
