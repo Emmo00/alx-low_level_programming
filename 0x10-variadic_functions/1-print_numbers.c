ff14b0503cc7d763dc31e463c50d838cf93ae851
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-01-20 17:08:03 +0100

    print all

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,6 +12,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
     unsigned int i;
 
     va_start(args, n);
+    if (separator == NULL)
+    {
+        return;
+    }
     for (i = 0; i < n; i++)
     {
         if (i != 0)
