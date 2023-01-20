56a2c1a187c9af2e9d73be4e462abe68fe71193f
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-01-20 16:41:47 +0100

    sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,6 +11,8 @@ int sum_them_all(const unsigned int n, ...)
     unsigned int i, sum;
 
     sum = 0;
+    if (n == 0)
+        return (0);
     va_start(args, n);
     for (i = 0; i < n; i++)
     {
