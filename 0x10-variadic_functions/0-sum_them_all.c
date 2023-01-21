cc647d28e3e24fcc3d2580c8b8830590f8e7e56b
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-01-21 11:33:23 -0800

    betty

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,18 +7,18 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
-    va_list args;
-    unsigned int i;
-    int sum;
+	va_list args;
+	unsigned int i;
+	int sum;
 
-    sum = 0;
-    if (n == 0)
-        return (0);
-    va_start(args, n);
-    for (i = 0; i < n; i++)
-    {
-        sum += va_arg(args, int);
-    }
-    va_end(args);
-    return (sum);
+	sum = 0;
+	if (n == 0)
+		return (0);
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
+	return (sum);
 }
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,27 +2,27 @@
 #include<stdarg.h>
 /**
  * print_numbers - print numbers, followed by a newline
- * @sepatator: the string to be printed between numbers
+ * @separator: the string to be printed between numbers
  * @n: number of integers passed to the function
  * Return: nothing
 */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    va_list args;
-    unsigned int i;
+	va_list args;
+	unsigned int i;
 
-    va_start(args, n);
-    if (separator == NULL)
-    {
-        return;
-    }
-    for (i = 0; i < n; i++)
-    {
-        if (i != 0)
-        {
-            printf("%s", separator);
-        }
-        printf("%d", va_arg(args, int));
-    }
-    printf("\n");
+	va_start(args, n);
+	if (separator == NULL)
+	{
+		return;
+	}
+	for (i = 0; i < n; i++)
+	{
+		if (i != 0)
+		{
+			printf("%s", separator);
+		}
+		printf("%d", va_arg(args, int));
+	}
+	printf("\n");
 }
