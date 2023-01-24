160f8683bf8df5319f682248c3d1a7c0abaa7053
Emmanuel <nwaforemmanuel005@gmail.com>
2023-01-24 12:19:19 +0100

    correct logic

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,8 +15,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	if (separator == NULL || n < 1)
 	{
-		printf("\n");
-		return;
+		*separator = "";
 	}
 	for (i = 0; i < n; i++)
 	{
