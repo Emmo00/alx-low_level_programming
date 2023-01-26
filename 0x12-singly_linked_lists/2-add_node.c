31291435a4d09f657f43b69f21d50a864120ef21
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-01-26 10:32:52 -0800

    adf

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,26 +7,21 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *p;
-	list_t *tmp;
 	list_t *new;
 
-	p = (list_t *)malloc(sizeof(list_t));
-	tmp = (list_t *)malloc(sizeof(list_t));
 	new = (list_t *)malloc(sizeof(list_t));
-	if (p == NULL || tmp == NULL || new == NULL)
+	if (new == NULL)
 		return (NULL);
 	new->str = strdup((char *)str);
 	new->len = strlen(str);
+	new->next = NULL;
 	if (*head == NULL)
 	{
-		*head = p;
-		(*head)->next = NULL;
+		*head = new;
+	} else
+	{
+		new->next = (*head)->next;
+		(*head)->next = new;
 	}
-	tmp = (*head)->next;
-	new->next = (*head)->next;
-	(*head)->next = new;
-	free(p);
-	free(tmp);
 	return (new);
 }
