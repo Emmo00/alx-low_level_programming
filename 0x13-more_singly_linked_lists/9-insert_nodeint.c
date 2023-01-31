58dc28fc2cc947783b71d4c7a5802eeaf0dba992
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-01-31 08:33:57 -0800

    insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,6 +16,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new->n = n;
 	node = *head;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 	for (i = 0; i < idx - 1; i++)
 	{
 		if (node == NULL)
