2ca0d27dd58fc061327c2c389c5221a7d8d98e24
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-02-07 07:28:05 -0800

    creat

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,10 +13,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		text_content = "";
-	fd = open(filename, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd < 0)
 		return (-1);
-	while (text_content != NULL)
+	while (*text_content != '\0')
 	{
 		if ((write(fd, text_content++, sizeof(char))) < 0)
 			return (-1);
