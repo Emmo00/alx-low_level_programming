8ff41376b92eada2fa4ede6441cf4920fb9bd12f
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-02-07 07:22:34 -0800

    create

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * create_file - creates a file and inputs string text_content in it
+ * @filename: name of the file
+ * @text_content: string to input in the file
+ * Return: 1 (success),  0(failed)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+	fd = open(filename, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+	if (fd < 0)
+		return (-1);
+	while (text_content != NULL)
+	{
+		if ((write(fd, text_content++, sizeof(char))) < 0)
+			return (-1);
+	}
+	return (1);
+}
