b034f6c6c68a6775b1c75f936ba14999336741cf
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-02-09 00:58:25 -0800

    cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,55 @@
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * main - entry point
+ * copies the content of a file to another
+ * @ac: argument count
+ * @av: argument vector
+ * Return: 0(success)
+ */
+int main(int ac, char **av)
+{
+	int fdr, fdw, nbuff;
+	char *file_from, *file_to, *buffer;
+
+	if (ac != 3)
+	{
+		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	file_from = av[1];
+	file_to = av[2];
+	fdr = open(file_from, O_RDONLY);
+	if (fdr < 0)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't read from file %s", file_from);
+		exit(98);
+	}
+	fdw = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 664);
+	if (fdw < 0)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't write to %s", file_to);
+		exit(99);
+	}
+	buffer = malloc(sizeof(char) * 1024);
+	do {
+		nbuff = read(fdr, buffer, sizeof(char) * 1024);
+		write(fdw, buffer, sizeof(char) * nbuff);
+	} while (nbuff > 0);
+	free(buffer);
+	if ((close(fdr)) == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't close fd %d", fdr);
+		exit(100);
+	}
+	if ((close(fdw)) == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't close fd %d", fdw);
+		exit(100);
+	}
+	return (0);
+}
