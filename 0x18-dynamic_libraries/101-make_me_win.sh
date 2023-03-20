c05e6d9001cf3ecf280edb7eec3c2a0d8e366de2
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-03-20 11:36:59 -0700

    hope

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -qP /tmp https://github.com/Emmo00/alx-low_level_programming/blob/main/0x18-dynamic_libraries/sure.o
-LD_PRELOAD=/tmp/sure.so
+wget -qP /tmp https://github.com/Emmo00/alx-low_level_programming/raw/main/0x18-dynamic_libraries/sure.so
+export LD_PRELOAD=/tmp/sure.so
--- 0x18-dynamic_libraries/sure.c
+++ 0x18-dynamic_libraries/sure.c
@@ -1,9 +1,9 @@
 int rand()
 {
     static int i = 0;
-    static int mine[6] = {9, 8, 10, 24, 75, 9};
+    static int mine[6] = {8, 8, 7, 9, 23, 74};
     if (i < 6)
 	    return (mine[i++]);
     else
-	    return (70);
+	    return (i * 16807 % 2147483647);
 }
