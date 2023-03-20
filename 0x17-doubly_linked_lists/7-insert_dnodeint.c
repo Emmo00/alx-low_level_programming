3c8540dd33a7dc05eab99c7ad336447204e318ab
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-03-20 01:29:57 +0100

    remove spaces

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,23 +9,23 @@
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *tmp, *node;
-    int i;
+	dlistint_t *tmp, *node;
+	int i;
 
-    node = malloc(sizeof(dlistint_t));
-    if (node == NULL || *h == NULL)
-        return (NULL);
-    node->n = n;
-    tmp = *h;
-    for (i = 0; i < idx - 1 && tmp; i++)
-    {
-        tmp = (tmp)->next;
-    }
-    if (i != --idx)
-        return (NULL);
-    node->prev = (tmp);
+	node = malloc(sizeof(dlistint_t));
+	if (node == NULL || *h == NULL)
+		return (NULL);
+	node->n = n;
+	tmp = *h;
+	for (i = 0; i < idx - 1 && tmp; i++)
+	{
+		tmp = (tmp)->next;
+	}
+	if (i != --idx)
+		return (NULL);
+	node->prev = (tmp);
 	node->next = tmp->next;
 	if (tmp != NULL)
 		(tmp)->next = node;
-    return (node);
+	return (node);
 }
