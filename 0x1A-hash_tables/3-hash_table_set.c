7073c4caa177cf1131cd040f2730ee753ec1436d
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-04-07 06:11:31 -0700

    ghl

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
-	if (*key == "")
+	if (strcmp("", key) == 0)
 		return (0);
 	node->key = strdup(key);
 	node->value = strdup(value);
