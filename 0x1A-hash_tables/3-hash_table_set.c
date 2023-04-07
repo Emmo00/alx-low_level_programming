56b5a8a6bc24c6f64e1e910ed23645d5e3c0f074
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-04-07 08:36:15 -0700

    more

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (node == NULL)
 		return (0);
 	if (strcmp("", key) == 0)
+	{
+		free(node);
 		return (0);
+	}
 	node->key = strdup(key);
 	node->value = strdup(value);
 	node->next = NULL;
@@ -23,18 +26,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if ((ht->array)[index] == NULL)
 	{
 		(ht->array)[index] = node;
+		return (1);
 	} else if (strcmp(key, (ht->array)[index]->key) == 0)
 	{
 		(ht->array)[index]->value = strdup(value);
 		free(node->key);
 		free(node->value);
 		free(node);
+		return (1);
 	} else
 	{
 		tmp = (ht->array)[index];
 		while (tmp->next != NULL)
 			tmp = tmp->next;
 		tmp->next = node;
+		return (1);
 	}
-	return (1);
+	free(node->key);
+	free(node->value);
+	free(node);
+	return (0);
 }
