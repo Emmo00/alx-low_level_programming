3274e429f2ff9b37db178f1d281cb04c4d99d1fd
Emmanuel Nwafor <88244468+Emmo00@users.noreply.github.com>
2023-04-07 21:10:05 +0100

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,14 +11,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node, *tmp;
 	unsigned long int index;
 
-	node = malloc(sizeof(hash_node_t));
-	if (node == NULL)
-		return (0);
-	if (strcmp("", key) == 0)
+if (ht == NULL)
+return 0;
+	if (strcmp("", key) == 0 || key == NULL)
 	{
-		free(node);
 		return (0);
 	}
+node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
 	node->key = strdup(key);
 	node->value = strdup(value);
 	node->next = NULL;
