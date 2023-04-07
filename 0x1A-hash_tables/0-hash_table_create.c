5b82824924853f67872ab4c5b6c0bf25dfbf5dee
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-04-07 00:37:30 -0700

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,8 +10,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size == 0)
 		return (NULL);
-	table = malloc(sizeof(hash_node_t) * size);
-	for (i = 0; i < size; i++)
-		table[i] = NULL;
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
 	return (table);
 }
