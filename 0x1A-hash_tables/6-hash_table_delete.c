e03ffa86d3a4cb407926f3f0494999a386b1a11f
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-04-10 18:42:24 +0100

    use iterative method

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,21 +1,5 @@
 #include "hash_tables.h"
 /**
- * free_hash_col - free hash column recur
- * @col: pointer to head of linked list
- * Return: nothing
- */
-void free_hash_col(hash_node_t *col)
-{
-	if (col == NULL)
-		return;
-	free_hash_col(col->next);
-	if (col->key != NULL)
-		free(col->key);
-	if (col->value != NULL)
-		free(col->value);
-	free(col);
-}
-/**
  * hash_table_delete - deletes a hash table.
  * @ht: pointer to hash table
  * Return: nothing
@@ -23,6 +7,7 @@ void free_hash_col(hash_node_t *col)
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
+	hash_node_t *node, *tmp;
 
 	if (ht == NULL)
 		return;
@@ -33,7 +18,15 @@ void hash_table_delete(hash_table_t *ht)
 	}
 	for (i = 0; i < ht->size; i++)
 	{
-		free_hash_col((ht->array)[i]);
+		node = (ht->array)[i];
+		while (node != NULL)
+		{
+			free(node->key);
+			free(node->value);
+			tmp = node->next;
+			free(node);
+			node = tmp;
+		}
 	}
 	free(ht->array);
 	free(ht);
