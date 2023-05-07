bec6d3d8e53acc9c8166c376afb5e816832910e9
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-05-07 01:02:17 +0100

    endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * get_endianness - checks the endianness of the machine
+ * Return: 0 if big endian, 1 if little
+ */
+int get_endianness(void)
+{
+	int n;
+	char *c;
+
+	n = 0x76543210;
+	c = &n;
+	if (*c == 0x10)
+		return (1);
+	else
+		return (0);
+}
